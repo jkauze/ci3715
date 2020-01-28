20106b4dead0eea6f3584e93556567e3def147ba
Jesus Kauze <12-10273@usb.ve>
2020-01-27 23:19:31 -0500

    Init del tester.py

---
+++ tarea2/tester.py
@@ -0,0 +1,16 @@
+# Archivo de prueba unitarias que debera pasar el programa
+# Los casos expuestos en este programa son casos maliciosos y bordes en el que el programa pudiera fallar
+# AUTORES:
+
+# David Segura #13-11341
+# Jesus Kauze #12-10273
+
+import unittest
+
+class testing(unittest.TestCase):
+
+    # CASO 1: Fecha supera los 140 anos de edad
+    def test1(self):
+
+if __name__ == '__main__':
+    unittest.main()
