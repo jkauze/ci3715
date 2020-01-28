113a9eda7625aeffcfd0a516ecfd9f6025546da5
Jesus Kauze <12-10273@usb.ve>
2020-01-27 23:49:43 -0500

    Init del programa pension

---
+++ tarea2/pension.py
@@ -0,0 +1,24 @@
+# PENSION JK & D10S
+
+# Función que devuelva como resultado si una persona cumple con
+# los requisitos legales para recibir una pensión de vejez del
+# Instituto Venezolano de los Seguros Sociales (IVSS).
+
+# AUTORES:
+
+# David Segura #13-11341
+# Jesus Kauze #12-10273
+
+# Manejo de fechas mediante liberia de python
+# Imports
+import datetime
+
+# Funcion para calcular la edad, Parametros: Fecha de nacimiento
+# Parametros: nacimiento : datetime (datetime.date(year,month,day))
+
+import datetime
+
+# Funcion para calcular la edad, Parametros: Fecha de nacimiento
+# Parametros: nacimiento : datetime (datetime.date(year,month,day))
+
+def CalcularEdad(nacimiento):
