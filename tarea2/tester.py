e4bbe30f148321379f4c17723039a5cd58a1ab4f
Jesus Kauze <12-10273@usb.ve>
2020-01-27 23:28:59 -0500

    Casos de prueba del calculo de edad

--- tarea2/tester.py
+++ tarea2/tester.py
@@ -6,11 +6,23 @@
 # Jesus Kauze #12-10273
 
 import unittest
+from pension.py import *
 
 class testing(unittest.TestCase):
 
+    #Casos de calculo de Edad
+
     # CASO 1: Fecha supera los 140 anos de edad
     def test1(self):
+        self.assertEqual(CalcularEdad(datetime.date(1794,12,13)), False)
+
+    # CASO 2: fecha invalida
+    def test2(self):
+        self.assertEqual(CalcularEdad(datetime.date(3000,122,13)), False)
+
+    # CASO 3: La fecha es correcta
+    def test3(self):
+        self.assertEqual(CalcularEdad(datetime.date(1994,12,13)), True)
 
 if __name__ == '__main__':
     unittest.main()
